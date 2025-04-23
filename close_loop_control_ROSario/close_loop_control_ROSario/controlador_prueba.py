fb92ba28161576f27d6a6d0ba6ad7d47d6e7850b
YonathanRA <a01737244@tec.mx>
2025-04-23 14:15:51 -0600

    Version casi chida

--- close_loop_control_ROSario/close_loop_control_ROSario/controlador_prueba.py
+++ close_loop_control_ROSario/close_loop_control_ROSario/controlador_prueba.py
@@ -14,12 +14,19 @@ class OpenLoopCtrl(Node):
         # Posiciones objetivo
         self.X_ref = 0.0
         self.Y_ref = 0.0
+        self.dist_ref = 0.0
         self.ang_ref = 0.0
 
         # Estados actuales
         self.X_robot = 0.0
         self.Y_robot = 0.0
         self.Th_robot = 0.0
+
+        self.dist_robot = 0.0
+
+        self.prev_X_robot = 0.0
+        self.prev_Y_robot = 0.0
+
 
         # PID angular
         self.kp_ang = 0.5
@@ -58,49 +65,82 @@ class OpenLoopCtrl(Node):
     def control_loop(self):
         if not self.robot_busy and self.path_queue:
             self.X_ref, self.Y_ref = self.path_queue.pop(0)
+
+            self.dist_ref = np.linalg.norm([self.X_ref - self.X_robot, self.Y_ref - self.Y_robot])
+
+            dx = self.X_ref - self.X_robot
+            dy = self.Y_ref - self.Y_robot
+            self.ang_ref = np.arctan2(dy, dx)
+
             self.robot_busy = True
             self.state = 0
             self.get_logger().info(f'Nuevo objetivo: ({self.X_ref}, {self.Y_ref})')
 
         if self.robot_busy:
             if self.state == 0:
-                # ROTACIÓN
-                dx = self.X_ref - self.X_robot
-                dy = self.Y_ref - self.Y_robot
-                self.ang_ref = np.arctan2(dy, dx)
+                
+                #self.get_logger().info(f"Ang_robot: {np.rad2deg(self.Th_robot)} | Ang obj: {np.rad2deg(self.ang_ref)}")
 
                 error = self.wrap_to_Pi(self.ang_ref - self.Th_robot)
                 self.get_logger().info(f"Error angular: {np.rad2deg(error):.2f}°")
 
-                if abs(error) < np.deg2rad(2.5):
+                if abs(error) < np.deg2rad(10.0):
+                    self.get_logger().info("LLEGO A LIM. ERROR ANGULAR")
                     self.twist.angular.z = 0.0
+                    
+                    self.integral_ang = 0.0
+                    self.prev_error_ang = 0.0
+
                     self.state = 1
+                    #self.get_logger().info(f"Estado dese angular: {self.state}.")
                     self.get_logger().info("Rotación completada.")
+                    self.cmd_vel_pub.publish(self.twist)
                 else:
+                    
                     output = self.pid_controller_angular(error)
+                    self.get_logger().info(f"Salida PID ANGULAR: {output}")
                     self.twist.angular.z = self.saturate_with_deadband(output, 0.29, 3.0)
-                    self.twist.linear.x = 0.0
+                    self.get_logger().info(f"V.ANG: {self.twist.angular.z}")
+                    #self.twist.linear.x = 0.0
+                    self.cmd_vel_pub.publish(self.twist)
 
             elif self.state == 1:
                 # TRASLACIÓN
-                dist = np.linalg.norm([self.X_ref - self.X_robot, self.Y_ref - self.Y_robot])
-                self.get_logger().info(f"Distancia al objetivo: {dist:.3f} m")
+                self.dist_robot += np.linalg.norm([self.X_robot - self.prev_X_robot, self.Y_robot - self.prev_Y_robot])
+                error = self.dist_ref - self.dist_robot
+                self.get_logger().info(f"Error lineal: {error:.3f} m")
+                self.prev_X_robot = self.X_robot
+                self.prev_Y_robot = self.Y_robot
 
-                if dist < 0.1:
+                if error < 0.1:
+                    self.get_logger().info("LLEGO A LIM. ERROR LINEAL")
                     self.twist.linear.x = 0.0
                     self.state = 2
+                    self.dist_robot = 0.0
+                    self.integral_lin = 0.0
+                    self.prev_error_lin = 0.0
+
+                    #self.get_logger().info(f"Estado desde lineal: {self.state}.")
                     self.get_logger().info("Traslación completada.")
+                    self.cmd_vel_pub.publish(self.twist)
                 else:
-                    output = self.pid_controller_lineal(dist)
+                    output = self.pid_controller_lineal(error)
+                    self.get_logger().info(f"Salida PID LINEAL: {output}")
                     self.twist.linear.x = self.saturate_with_deadband(output, 0.025, 0.38)
-                    self.twist.angular.z = 0.0
+                    self.get_logger().info(f"V.LIN: {self.twist.linear.x}")
+                    #self.twist.angular.z = 0.0
+                    self.cmd_vel_pub.publish(self.twist)
 
             elif self.state == 2:
-                self.twist = Twist()
+                #self.twist = Twist()
+                self.twist.linear.x = 0.0
+                self.twist.angular.z = 0.0
+
                 self.robot_busy = False
                 self.get_logger().info("Objetivo alcanzado. Esperando siguiente...")
+                self.cmd_vel_pub.publish(self.twist)
 
-            self.cmd_vel_pub.publish(self.twist)
+            
 
     def odometria(self, msg):
         self.X_robot = msg.pose.pose.position.x
@@ -130,8 +170,11 @@ class OpenLoopCtrl(Node):
 
     def saturate_with_deadband(self, output, min_val, max_val):
         if abs(output) < min_val:
-            return 0.0
-        return np.clip(output, -max_val, max_val)
+            return min_val
+        elif output > 0:
+            return min(output,max_val)
+        else:
+            return max(output, -max_val)
 
 def main(args=None):
     rclpy.init(args=args)

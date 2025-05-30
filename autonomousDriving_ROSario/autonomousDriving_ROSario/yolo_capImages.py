67e86d1b2c488df21b9421c594994ef3f813d5c4
oscar_ot09 <oscar_ot09@outlook.es>
2025-05-30 14:45:23 -0600

    Launch captura de fotos

--- autonomousDriving_ROSario/autonomousDriving_ROSario/yolo_capImages.py
+++ autonomousDriving_ROSario/autonomousDriving_ROSario/yolo_capImages.py
@@ -1,5 +1,5 @@
 def main():
-    print('Hi from autonomousDriving_ROSario.')
+    print('Hi from yolo_capImages.')
 
 
 if __name__ == '__main__':
--- autonomousDriving_ROSario/setup.py
+++ autonomousDriving_ROSario/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'autonomousDriving_ROSario'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), # Launch File
+        glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
